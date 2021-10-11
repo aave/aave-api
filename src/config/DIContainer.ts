541505befbe70d931bdf8305300cf25a1eb0d257
sakulstra <lukasstrassel@googlemail.com>
2021-10-11 14:54:28 +0200

    fix: remove permissions api

--- src/config/DIContainer.ts
+++ src/config/DIContainer.ts
@@ -5,13 +5,6 @@ import MongoDBInterface from '../interfaces/MongoDB';
 
 import { MONGO_URL } from './Environment';
 import { gqlSdkV2 } from '../repositories/subgraph';
-import addressListingModel from '../repositories/mongodb/models/AddressListings';
-import AddressListingsDomain, {
-  AddressListingsDomainInterface,
-} from '../repositories/mongodb/domains/addressListings';
-import PermissionManagerService, {
-  PermissionManagerInterface,
-} from '../services/contracts/PermissionManager';
 import RateDomain, {
   RatesDomainInterface,
 } from '../repositories/mongodb/domains/Rate';
@@ -23,21 +16,9 @@ const container = new Container();
 const mongoDBInstance: MongoDBInterface = new MongoDB({ url: MONGO_URL });
 
 const rateDomain: RatesDomainInterface = new RateDomain(rateModel);
-const addressListingDomain: AddressListingsDomainInterface = new AddressListingsDomain(
-  addressListingModel,
-);
 
 container.bind('SubgraphClientV2').toConstantValue(gqlSdkV2);
 container.bind('MongoDBClient').toConstantValue(mongoDBInstance);
-container.bind('AddressListingDomain').toConstantValue(addressListingDomain);
 container.bind('RateDomain').toConstantValue(rateDomain);
-
-const permissionManagerServiceInstance: PermissionManagerInterface = container.resolve(
-  PermissionManagerService,
-);
-
-container
-  .bind('PermissionManagerService')
-  .toConstantValue(permissionManagerServiceInstance);
 
 export default container;
--- src/repositories/blockchain/connection.ts
+++
@@ -1,8 +0,0 @@
-import { providers } from 'ethers';
-import { ChainId } from '@aave/protocol-js';
-import { JSONRPC_URL, NETWORK } from '../../config/Environment';
-
-export const provider = new providers.JsonRpcProvider(
-  JSONRPC_URL,
-  ChainId[NETWORK],
-);
